ec2888d2733309105f4ce071009456c2ab8df446
Kevin Carrasco <itzvin19@gmail.com>
2025-10-22 16:27:22 -0500

    galeria

---
+++ src/data/constants.ts
@@ -0,0 +1,11 @@
+export const constants = {
+    SITE_NAME: "Dance Studio",
+    CONTACT_EMAIL: "info@dancepoint.com",
+    CONTACT_PHONE: "(01) 059-548",
+    ADDRESS: "Av. Los Angeles 123, Lima, Peru",
+    YOUTUBE_URL: "https://www.youtube.com/dancestudio",
+    FACEBOOK_URL: "https://www.facebook.com/dancestudio",
+    INSTAGRAM_URL: "https://www.instagram.com/dancestudio",
+    X_URL: "https://www.x.com/dancestudio",
+    WHATSAPP_URL: "https://wa.me/1234567890",
+}
